1a496c23c7f217f5799e4fc85c8e7d42c7be4d2a
Jia An Wong <JiaAn@Jia-Ans-MacBook-Pro.local>
2019-03-01 13:59:49 +0800

    add initial mode prototype

---
+++ GrinoldKronerModelFinal.py
@@ -0,0 +1,119 @@
+import good_morning as gm
+import pandas_datareader.data as web
+import bs4 as bs
+import urllib.request
+import pandas as pd
+import numpy as np
+import math
+import time
+pd.set_option('display.expand_frame_repr', False)
+
+# kr = gm.KeyRatiosDownloader()
+# kr_frames = kr.download('AAPL')
+# print(kr_frames)
+
+# kr_keyfin = kr_frames[0].transpose()
+# print(kr_keyfin)
+
+closingpricedates = ['2010-12-31','2010-12-31','2011-12-30','2012-12-31','2013-12-31','2014-12-31','2015-12-31','2016-12-30','2017-12-29','2018-12-31']
+
+def getAverageRevG(kr_keyfin, years):
+
+    kr_keyfin['PctRevenueChange'] = kr_keyfin['Revenue USD Mil'].pct_change()
+    #print(str(years) + " year average revenue growth is " + str(kr_keyfin['PctRevenueChange'].iloc[-1]*100) + "%")
+    averageRevG = kr_keyfin['PctRevenueChange'].rolling(years).mean().iloc[-1]*100
+    if np.isnan(averageRevG):
+        averageRevG = 0
+    return averageRevG
+
+# data = web.get_data_yahoo('AAPL','20/12/2010', interval='d')['Adj Close']
+# print(data)
+
+def addPrices(kr_keyfin, stock, datelist):
+    data = web.get_data_yahoo(stock,'01/01/2009', interval='d')['Adj Close']
+    #print(data)
+    yearEndPrices =[]
+    for day in datelist:
+        yearEndPrices.append(data.loc[day])
+    yearEndPrices.append(0) #have to eventually tune to check if recent year's financials are available
+    #print(yearEndPrices)
+    kr_keyfin['Closing Prices'] = yearEndPrices
+        
+def getAverageDivYield(kr_keyfin, years):
+    kr_keyfin['Dividend Yield'] =  (kr_keyfin['Dividends USD'] / kr_keyfin['Closing Prices'])
+    #print( str(years) + ' year average dividend yield is ' + str ( kr_keyfin['Dividend Yield'].rolling(years).mean().iloc[-2]*100 ) + '%' )
+    AverageDivYield = kr_keyfin['Dividend Yield'].rolling(years).mean().iloc[-2]*100
+    if np.isnan(AverageDivYield):
+        AverageDivYield = 0
+    return AverageDivYield
+
+def getChangeInSO(kr_keyfin, years):
+    kr_keyfin['Change in SO'] = kr_keyfin['Shares Mil'].pct_change()
+    #print( str(years) + ' year average change in SO ' + str ( kr_keyfin['Change in SO'].rolling(years).mean().iloc[-2]*100 ) + '%' )
+    changeInSo = kr_keyfin['Change in SO'].rolling(years).mean().iloc[-2]*100
+    if np.isnan(changeInSo):
+        changeInSo = 0
+    return changeInSo
+
+def getAnnMultiples(stock, inhorizon):
+    sauce = urllib.request.urlopen('https://www.macrotrends.net/stocks/charts/' + stock + '/nvidia/pe-ratio').read()
+    soup = bs.BeautifulSoup(sauce,'lxml')
+    
+    table  = soup.find_all('table',class_='table')[0]
+    df = pd.read_html(str(table))[0]
+    latestPE = df['Unnamed: 3'].iloc[1]
+    medianPE = df['Unnamed: 3'].median()
+    multiplesExCon  = (medianPE / latestPE - 1)*100
+    annualExCon = multiplesExCon / inhorizon
+    if np.isnan(annualExCon):
+        annualExCon = 0
+    return annualExCon
+
+def GrinoldKroner (stock, years, inhorizon, datelist):
+    
+    kr = gm.KeyRatiosDownloader()
+    kr_frames = kr.download(stock)
+    # print(kr_frames)
+    kr_keyfin = kr_frames[0].transpose()
+    # print(kr_keyfin)
+
+    addPrices(kr_keyfin,stock,datelist)
+    gk_revenueGrowth = getAverageRevG(kr_keyfin, years)
+    gk_divYield = getAverageDivYield(kr_keyfin, years)
+    gk_changeInSo = getChangeInSO(kr_keyfin, years)
+    gk_annualExCon = getAnnMultiples(stock, inhorizon)
+
+    print('-------------------- ANALYSIS OF ' + stock + ' --------------------')
+    print( str(years) + ' historical year average revenue growth is ' + str(gk_revenueGrowth) + '%')
+    print( str(years) + ' historical year average dividend yield is ' + str (gk_divYield) + '%' )
+    print( str(years) + ' historical year average change in shares outstanding ' + str (gk_changeInSo) + '%' )
+    print( 'Annual Multiples change over next ' + str(inhorizon) + ' yeaars is ' + str (gk_annualExCon) + '%' )
+    print('')
+    returns = gk_revenueGrowth + gk_divYield - gk_changeInSo + gk_annualExCon
+
+    print('Expected returns for ' + stock + ' is ' + str(returns) + '%')
+    print('')
+    print('')
+
+    #print(kr_keyfin)
+
+def GrinoldKronerList(stocklist, years, inhorizon, datelist):
+    for stock in stocklist:
+        GrinoldKroner(stock, years, inhorizon, datelist)
+        time.sleep(5)
+
+
+stockListTest = ['AAPL','NVDA','AMAT']
+stockListSemi = ['AVGO','INTC','MRVL','MU','NVDA']
+
+#GrinoldKroner('MU',3,7,closingpricedates)
+
+GrinoldKronerList(stockListSemi,3,7,closingpricedates)
+
+# kf = gm.FinancialsDownloader()
+# kf_frames = kf.download('AAPL')
+
+# print(kf_frames[2009])
+
+# data = web.get_data_yahoo('IBM','01/01/2015', interval='d')
+# print(data.columns())#
