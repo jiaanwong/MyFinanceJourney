1916a564e412d8f9c7e56d06d36693255325d009
jiaanwong <wongjiaan@gmail.com>
2019-03-02 12:09:07 +0800

    add function to display key financials of interest, add option to round  to specific decimal places, drop innacurate forecasted revenue for following FY

--- GrinoldKronerModelFinal.py
+++ GrinoldKronerModelFinal.py
@@ -1,4 +1,3 @@
-import good_morning as gm
 import pandas_datareader.data as web
 import bs4 as bs
 import urllib.request
@@ -6,6 +5,7 @@ import pandas as pd
 import numpy as np
 import math
 import time
+import good_morning as gm
 
 # Function to add closing prices to key financials data frame
 def addPrices(kr_keyfin, stock, datelist):
@@ -16,7 +16,8 @@ def addPrices(kr_keyfin, stock, datelist):
     yearEndPrices.append(0) #have to eventually tune to check if recent year's financials are available
     kr_keyfin['Closing Prices'] = yearEndPrices
         
-# Function to retrieve analyst consensus for revenue growth
+# Function to include analyst consensus for revenue growth
+# To further refine in future by using long term growth
 def getConRevG(stock):
     sauce = urllib.request.urlopen('https://finance.yahoo.com/quote/' + stock + '/analysis/')
     soup = bs.BeautifulSoup(sauce,'lxml')
@@ -35,7 +36,7 @@ def getAverageRevG(kr_keyfin, years):
     if np.isnan(historicalRevG):
         historicalRevG = 0
     return historicalRevG
-# To add estimation of margin expansions to complete earnings growth component
+    # To add estimation of margin expansions to complete earnings growth component
 
 # Function to return average dividend yield 
 def getAverageDivYield(kr_keyfin, years):
@@ -57,7 +58,6 @@ def getChangeInSO(kr_keyfin, years):
 def getAnnMultiples(stock, inhorizon):
     sauce = urllib.request.urlopen('https://www.macrotrends.net/stocks/charts/' + stock + '/nvidia/pe-ratio').read()
     soup = bs.BeautifulSoup(sauce,'lxml')
-    
     table  = soup.find_all('table',class_='table')[0]
     df = pd.read_html(str(table))[0]
     latestPE = df['Unnamed: 3'].iloc[1]
@@ -68,56 +68,91 @@ def getAnnMultiples(stock, inhorizon):
         annualExCon = 0
     return annualExCon
 
+def displayKeyFinancials(kr_frames):
+    #Calculating Revenue Growth
+    keyfinancials = kr_frames[0].drop(kr_frames[0].index[1:])
+    revenueGrowth = kr_frames[0].iloc[0].pct_change()*100
+    revenueGrowth.name = 'Revenue Growth'
+    keyfinancials = keyfinancials.append(revenueGrowth)
+    #Profitability Measures
+    keyfinancials = keyfinancials.append(kr_frames[2].loc['Return on Invested Capital %']) #ROIC
+    keyfinancials = keyfinancials.append(kr_frames[0].iloc[[3,12]]) #Operating Margin and FCF
+    #Ability to maintain dividend payout
+    totalDividends = kr_frames[0].iloc[6] *  kr_frames[0].iloc[8]
+    totalDividends.name = 'Total Dividends'
+    keyfinancials = keyfinancials.append(totalDividends)
+    #Degree of leverage
+    keyfinancials = keyfinancials.append(kr_frames[9].iloc[[2]]) #Operating Margin and FCF
+    #Cleaning up table for display
+    keyfinancials.drop( keyfinancials.columns[len(keyfinancials.columns)-1],axis = 1, inplace=True) #drops inaccurate forecasted data 
+    keyfinancials = np.round(keyfinancials,decimals=2) #rounds to 2 decimals
+    print(keyfinancials)
+
 
 # Function to calculate overall expected returns based on the Grinold and Kroner Model
-def GrinoldKroner (stock, years, inhorizon, datelist):
+def GrinoldKroner (stock, years, inhorizon, datelist,roundDec):
     
-    print('-------------------- ANALYSIS OF ' + stock + ' --------------------')
+    print('------------------------- ANALYSIS OF ' + stock + ' -------------------------')
+    # Initial download of data key financials
     kr = gm.KeyRatiosDownloader()
     kr_frames = kr.download(stock)
     kr_keyfin = kr_frames[0].transpose()
     addPrices(kr_keyfin,stock,datelist)
 
-    # Further refining of decision process for best revenue growth 
-    print('- ESTIMATING REVENUE GROWTH -')
+    # Estimating revenue growth (to further refine decision process)
+    print('\n- ESTIMATING REVENUE GROWTH -')
     gk_revenueGrowth = getAverageRevG(kr_keyfin, years)
-    print( "   " + str(years) + ' historical year average revenue growth is ' + str(gk_revenueGrowth) + '%')
-    gk_consensusGrowth = getConRevG(stock)
-    print( '   Analyst consensus growth rate of ' + str(gk_consensusGrowth) + '%')
+    print(str(years) + ' historical year average revenue growth is ' + str(round(gk_revenueGrowth,roundDec
+)) + '%')
+    gk_consensusGrowth = round(getConRevG(stock),roundDec
+)
+    print('Analyst consensus growth rate of ' + str(gk_consensusGrowth) + '%')
     if gk_consensusGrowth > gk_revenueGrowth:
         gk_conservativeGrowth = gk_revenueGrowth
-        print('> Conservative growth rate is set to historical growth rate of ' + str(gk_conservativeGrowth))
+        print('> Conservative growth rate is set to historical growth rate of ' + str(gk_conservativeGrowth) + '%')
     else:
         gk_conservativeGrowth = gk_consensusGrowth
-        print('> Conservative growth rate is set to analyst consensus of ' + str(gk_conservativeGrowth))
+        print('> Conservative growth rate is set to analyst consensus of ' + str(gk_conservativeGrowth) + '%')
 
+    # Calculation of other components
     gk_divYield = getAverageDivYield(kr_keyfin, years)
     gk_changeInSo = getChangeInSO(kr_keyfin, years)
     gk_annualExCon = getAnnMultiples(stock, inhorizon)
 
+    # Displaying overall findings
+    print('\n- ESTIMATING DIVIDEND YIELD, CHANGE IN SO AND MULTIPLES EXPANSION -')
+    print( '> historical average dividend yield is ' + str (round(gk_divYield,roundDec
+)) + '%' )
+    print( '> historical average change in shares outstanding ' + str (round(gk_changeInSo,roundDec
+)) + '%' )
+    print( '> Annual Multiples change over next ' + str(inhorizon) + ' yeaars is ' + str (round(gk_annualExCon,roundDec
+)) + '%' )
     print('')
-    print('- OTHER ESTIMATES -')
-    print( str(years) + ' historical year average dividend yield is ' + str (gk_divYield) + '%' )
-    print( str(years) + ' historical year average change in shares outstanding ' + str (gk_changeInSo) + '%' )
-    print( 'Annual Multiples change over next ' + str(inhorizon) + ' yeaars is ' + str (gk_annualExCon) + '%' )
-    print('')
-    returns = gk_conservativeGrowth + gk_divYield - gk_changeInSo + gk_annualExCon
+    expectedreturns = gk_conservativeGrowth + gk_divYield - gk_changeInSo + gk_annualExCon
+    print('>>> Final expected returns based on the Grinold & Kroner Model for ' + stock + ' is ' + str(round(expectedreturns,roundDec
+)) + '% \n')
 
-    print('Expected returns for ' + stock + ' is ' + str(returns) + '%')
-    print('')
-    print('')
+    # Displays selected important financials
+    print('\n- SELECTED KEY FINANCIALS -')
+    displayKeyFinancials(kr_frames)
+    print('\n' *2)
 
 
 # Allows for input of multiple stock tickers
-def GrinoldKronerList(stocklist, years, inhorizon, datelist):
+def GrinoldKronerList(stocklist, years, inhorizon, datelist, roundDec):
     for stock in stocklist:
-        GrinoldKroner(stock, years, inhorizon, datelist)
-        time.sleep(5)
+        GrinoldKroner(stock, years, inhorizon, datelist, roundDec)
+        time.sleep(10)
 
 
-# Sample Inputs
-closingpricedates = ['2010-12-31','2010-12-31','2011-12-30','2012-12-31','2013-12-31','2014-12-31','2015-12-31','2016-12-30','2017-12-29','2018-12-31']
-stockListTest = ['AAPL','NVDA','AMAT']
-stockListElec = ['AVGO','INTC','MRVL','MU','NVDA']
+# # Sample Parameters and Input
+# histAvgYears = 3
+# investHorizon = 7
+# roundedDecimal = 4
+# closingpricedates = ['2010-12-31','2010-12-31','2011-12-30','2012-12-31','2013-12-31','2014-12-31','2015-12-31','2016-12-30','2017-12-29','2018-12-31']
+# stockListShort = ['AAPL','NVDA','AMAT']
+# stockListElec = ['MU','NVDA','TXN','QCOM','AVGO','INTC','MRVL']
+# pd.set_option('display.expand_frame_repr', False)
 
-GrinoldKronerList(stockListElec,3,7,closingpricedates)
+
+# GrinoldKronerList(stockListElec,histAvgYears,investHorizon,closingpricedates,roundedDecimal)
